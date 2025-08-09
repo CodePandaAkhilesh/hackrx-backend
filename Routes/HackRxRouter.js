d02cbfb004c80f956368095ca82cd84b3d97f0e5
Akhilesh Verma <152074281+CodePandaAkhilesh@users.noreply.github.com>
2025-08-09 17:19:35 +0530

    Update HackRxRouter.js

--- Routes/HackRxRouter.js
+++ Routes/HackRxRouter.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 // Initialize Gemini model
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
+const model = genAI.getGenerativeModel({ model: "gemini-2.5-pro" });
 
 // Normalize spaces
 const preprocessText = (text) => text.replace(/\s+/g, " ").trim();
